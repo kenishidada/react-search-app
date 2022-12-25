4628e5a498b2612b7ae7f4d5a48afa5e59913d07
KenIshida <kenishida95@gmail.com>
2022-12-25 14:32:13 +0900

    トップページの作成、検索機能の実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import './App.css';
+import { User } from './types/User';
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [filteredUserList, setFilteredUserList] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setUsers(data);
+        setFilteredUserList(data);
+      })
+      .then(() => {});
+  }, []);
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    const searchWord = e.target.value.toLowerCase();
+    setFilteredUserList(users.filter((user: User) => user.name.toLowerCase().includes(searchWord)));
+  };
+
   return (
-    <div className="text-center pt-16 underline">
-      <h2>Hello React!</h2>
+    <div className=" text-center">
+      <nav className={'p-6'}>
+        <a href={'/'} className={'text-lg font-bold'}>
+          名前検索アプリ
+        </a>
+        <hr />
+      </nav>
+      <input
+        type="text"
+        onChange={handleSearch}
+        className={'p-2 ml-10 mr-10 mt-3 w-3/4'}
+        placeholder={'名前検索'}
+      />
+      <div className={"flex justify-center flex-wrap gap-4 pt-10"}>
+        {filteredUserList.map((user: User) => {
+          return (
+            <div key={user.id} className={"rounded-xl border border-blue-200 hover:bg-blue-200 shadow-lg w-1/3"}>
+              <a href={'/'} >
+                <div className={'p-4 space-y-2'}>
+                  <p className={'text-xl font-medium'}>{user.name}</p>
+                  <p className={'text-slate-500'}> email: {user.email} </p>
+                  <p className={'text-slate-700'}>site: {user.website}</p>
+                </div>
+              </a>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
