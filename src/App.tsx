fa75ab1bedeeee4a7a9821b946cceb5480ed5e0b
KenIshida <kenishida95@gmail.com>
2022-12-26 14:37:24 +0900

    cssの調整

--- src/App.tsx
+++ src/App.tsx
@@ -34,20 +34,18 @@ function App() {
       <input
         type="text"
         onChange={handleSearch}
-        className={'p-2 ml-10 mr-10 mt-3 w-3/4'}
+        className={'p-2 ml-10 mr-10 mt-3 w-3/4 border border-blue-200'}
         placeholder={'名前検索'}
       />
       <div className={"flex justify-center flex-wrap gap-4 pt-10"}>
         {filteredUserList.map((user: User) => {
           return (
             <div key={user.id} className={"rounded-xl border border-blue-200 hover:bg-blue-200 shadow-lg w-1/3"}>
-              <a href={'/'} >
                 <div className={'p-4 space-y-2'}>
                   <p className={'text-xl font-medium'}>{user.name}</p>
                   <p className={'text-slate-500'}> email: {user.email} </p>
                   <p className={'text-slate-700'}>site: {user.website}</p>
                 </div>
-              </a>
             </div>
           );
         })}
